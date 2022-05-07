83d63d0f2226300371997c4fa08daaa9c7c442b7
Adriano <adriano_alves_2008@hotmail.com>
2022-05-07 18:48:15 -0300

    resolvendo Json

--- assets/js/services/getHomeContent.js
+++ assets/js/services/getHomeContent.js
@@ -1,6 +1,6 @@
 export const getHomeContent = () => {
   return new Promise((resolve, reject) => {
-    fetch('/content/data.json')
+    fetch('/Clone-Disney/content/data.json')
       .then(response => {
         return response.json()
       })
